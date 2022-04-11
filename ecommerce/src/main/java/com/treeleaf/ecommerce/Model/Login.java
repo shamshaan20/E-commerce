43c2ecdf6acc20e38949dddd9f032727b9625803
shamshaan20 <shamshaan20@gmail.com>
2022-04-11 12:50:09 +0545

    Project First Commit

---
+++ ecommerce/src/main/java/com/treeleaf/ecommerce/Model/Login.java
@@ -0,0 +1,38 @@
+package com.treeleaf.ecommerce.Model;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "LoginUser")
+public class Login {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String userName;
+    private String password;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
